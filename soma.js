c5563282f9fd38b591e218fcd63da292014b9b86
emanuel0711 <emanuelchaves321@gmail.com>
2025-07-07 20:07:01 -0300

    Delete soma.js

--- soma.js
+++
@@ -1,8 +0,0 @@
-// 1. Função de soma
-function somaNumeros(a, b) {
-  return a + b;
-}
-
-// 2. Chamada e exibição
-const resultado = somaNumeros(5, 7);
-console.log("O resultado da soma é:", resultado);
